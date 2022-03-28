35e0078166772cfc295e7bb3314f9ee72a83695c
utso1707097 <utsomollik1707097@gmail.com>
2022-03-28 18:23:15 +0600

    Drawer Layout added

--- app/src/main/java/com/utsobro/hellodoctor/ProfileActivity.java
+++ app/src/main/java/com/utsobro/hellodoctor/ProfileActivity.java
@@ -1,50 +1,95 @@
 package com.utsobro.hellodoctor;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.transition.Slide;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
-
-    private TextView showEmail;
-    private FirebaseAuth firebaseAuth;
-    private Button logoutButton;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        showEmail = findViewById(R.id.showEmail);
-        firebaseAuth = FirebaseAuth.getInstance();
-        logoutButton = findViewById(R.id.logoutButton);
-        checkUser();
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.toolbar);
 
-        logoutButton.setOnClickListener(new View.OnClickListener() {
+        //toolbar setup
+
+        setSupportActionBar(toolbar);
+
+        //action bar toggle
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(ProfileActivity.this,drawerLayout,toolbar,R.string.openDrawer,R.string.closeDrawer);
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                firebaseAuth.signOut();
-                checkUser();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id == R.id.optProfile){
+                    Toast.makeText(ProfileActivity.this,"Profile",Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.optAppointments){
+                    Toast.makeText(ProfileActivity.this,"Appointments",Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.optDoctors){
+                    Toast.makeText(ProfileActivity.this,"Doctors",Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.optEmergency){
+                    Toast.makeText(ProfileActivity.this,"Emergency",Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.optAmbulance){
+                    Toast.makeText(ProfileActivity.this,"Ambulance",Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.optAboutUs){
+                    Toast.makeText(ProfileActivity.this,"AboutUs",Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.optLogOut){
+                    Toast.makeText(ProfileActivity.this,"LogOut",Toast.LENGTH_SHORT).show();
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+
+
+                return true;
             }
         });
-
     }
-    void checkUser(){
-        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        if(firebaseUser == null){
-            startActivity(new Intent(ProfileActivity.this,LoginActivity.class));
-            finish();
+
+    //on back press
+
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
         }
         else{
-            String email = firebaseUser.getEmail();
-            showEmail.setText(email);
+            //app closed
+            super.onBackPressed();
         }
     }
 }
