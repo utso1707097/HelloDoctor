925de159569cbe219dcc33ab8075100b4692985a
utso1707097 <utsomollik1707097@gmail.com>
2022-03-30 17:40:20 +0600

    Registration page designed successfully

--- app/src/main/java/com/utsobro/hellodoctor/RegisterActivity.java
+++ app/src/main/java/com/utsobro/hellodoctor/RegisterActivity.java
@@ -1,15 +1,22 @@
 package com.utsobro.hellodoctor;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +28,10 @@ import com.google.android.material.badge.BadgeUtils;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.UUID;
 
 public class RegisterActivity extends AppCompatActivity {
     private TextView goBackLogin;
@@ -28,6 +39,17 @@ public class RegisterActivity extends AppCompatActivity {
     private Button registerBtn;
     private FirebaseAuth firebaseAuth;
     private ProgressDialog progressDialog;
+    //for profile picture
+    private ImageView profilePic;
+    private final int GALLERY_REQ_CODE=1000;
+    private Uri imageUri;
+    private FirebaseStorage firebaseStorage;
+    private StorageReference storageReference;
+
+    //Doctor registration
+    private Switch switchDoctor;
+    private Spinner hospitalName,expertIn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,10 +62,59 @@ public class RegisterActivity extends AppCompatActivity {
         retypePassword = findViewById(R.id.retypePassword);
         registerBtn = findViewById(R.id.registerBtn);
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseStorage=FirebaseStorage.getInstance();
+
+
         progressDialog = new ProgressDialog(RegisterActivity.this);
         progressDialog.setTitle("Please wait");
         progressDialog.setMessage("Creating your account");
         progressDialog.setCanceledOnTouchOutside(false);
+
+        //profile pic
+        firebaseStorage = FirebaseStorage.getInstance();
+        profilePic = findViewById(R.id.profilePic);
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iGallery =new Intent(Intent.ACTION_PICK);
+                //access internal storage to external storage
+                iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(iGallery,GALLERY_REQ_CODE);
+
+            }
+        });
+        //Doctor registration
+        switchDoctor = findViewById(R.id.switchDoctor);
+        hospitalName = findViewById(R.id.hospitalName);
+        expertIn = findViewById(R.id.expertIn);
+
+        //Set up the hospital spinner
+        String[] hospitalNames =getResources().getStringArray(R.array.hospital_names);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(RegisterActivity.this, android.R.layout.simple_spinner_item,hospitalNames);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        hospitalName.setAdapter(arrayAdapter);
+
+        //set up the expertIn spinner
+        String[] expertiseFields =getResources().getStringArray(R.array.expertise_fileds);
+        ArrayAdapter arrayAdapter1 = new ArrayAdapter(RegisterActivity.this, android.R.layout.simple_spinner_item,expertiseFields);
+        arrayAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        expertIn.setAdapter(arrayAdapter1);
+
+        switchDoctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(switchDoctor.isChecked()){
+                    hospitalName.setVisibility(View.VISIBLE);
+                    expertIn.setVisibility(View.VISIBLE);
+                }
+                else{
+                    hospitalName.setVisibility(View.GONE);
+                    expertIn.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
 
         //go back to login
         goBackLogin = findViewById(R.id.goBackLogin);
@@ -99,11 +170,10 @@ public class RegisterActivity extends AppCompatActivity {
                 //get user info
                 FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
                 String email = firebaseUser.getEmail();
-                Toast.makeText(RegisterActivity.this,"account created \n"+email,Toast.LENGTH_SHORT).show();
+                Toast.makeText(RegisterActivity.this,"account created with\n"+email,Toast.LENGTH_SHORT).show();
                 //open profile activity
                 startActivity(new Intent(RegisterActivity.this,ProfileActivity.class));
                 finish();
-
             }
         })
                 .addOnFailureListener(new OnFailureListener() {
@@ -115,4 +185,17 @@ public class RegisterActivity extends AppCompatActivity {
                     }
                 });
     }
+    //Profile Picture setup
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLERY_REQ_CODE && resultCode == RESULT_OK && data != null && data.getData()!=null){
+            imageUri = data.getData();
+            profilePic.setImageURI(imageUri);
+        }
+    }
+    
+
 }
